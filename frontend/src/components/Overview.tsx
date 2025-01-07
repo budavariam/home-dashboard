f5658192709b74f66ff0730998aabbe43ac71a4e
Mátyás Budavári <budavariam@gmail.com>
2025-01-07 22:37:52 +0100

    remove debugger

--- frontend/src/components/Overview.tsx
+++ frontend/src/components/Overview.tsx
@@ -59,7 +59,6 @@ const Overview = ({ isFetching, data, mappings, refetch }: OverviewProps) => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {data?.map((sensor) => {
-          debugger
           const mappedName = mappings[sensor.n] || sensor.n;
           const date = new Date(sensor.ts);
           const formattedTime = date.toLocaleTimeString();
